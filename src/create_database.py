1f59f35ad602707c2b659873718cf4ac38649243
Etienne GABORIAU <E155693G@U-INF-J-C002-09.iut-nantes.univ-nantes.prive>
2017-03-16 11:16:51 +0100

    adaptation base

--- src/create_database.py
+++ src/create_database.py
@@ -4,8 +4,8 @@ try:
     conn = mysql.connect(user='E155693G',password='E155693G',host='infoweb', database='E155693G')
     cursor = conn.cursor()
     cursor.execute("CREATE TABLE installations (\
-                        num_instal int(10) DEFAULT NULL,\
-                        nom varchar(50) DEFAULT NULL,\
+                        num_instal int(20) DEFAULT NULL,\
+                        nom varchar(500) DEFAULT NULL,\
                         code_postal int(5) DEFAULT NULL,\
                         ville varchar(40) DEFAULT NULL,\
                         latitude decimal(23,20) DEFAULT NULL,\
@@ -13,20 +13,20 @@ try:
                         PRIMARY KEY(num_instal)\
                     )")
     cursor.execute("CREATE TABLE equipements (\
-                        id_equip int(10) DEFAULT NULL,\
+                        id_equip int(20) DEFAULT NULL,\
                         nom varchar(50) DEFAULT NULL,\
-                        num_instal int(5) DEFAULT NULL,\
+                        num_instal int(20) DEFAULT NULL,\
                         PRIMARY KEY(id_equip),\
                         FOREIGN KEY(num_instal) REFERENCES installations(num_instal)\
                     )")
     cursor.execute("CREATE TABLE activites (\
-                        id_act int(10) DEFAULT NULL,\
-                        nom varchar(50) DEFAULT NULL,\
+                        id_act int(20) DEFAULT NULL,\
+                        nom varchar(500) DEFAULT NULL,\
                         PRIMARY KEY(id_act)\
                     )")
     cursor.execute("CREATE TABLE equip_act (\
-                        id_equip int(10) DEFAULT NULL,\
-                        id_act int(10) DEFAULT NULL,\
+                        id_equip int(20) DEFAULT NULL,\
+                        id_act int(20) DEFAULT NULL,\
                         PRIMARY KEY(id_equip, id_act),\
                         FOREIGN KEY(id_equip) REFERENCES equipements(id_equip),\
                         FOREIGN KEY(id_act) REFERENCES activites(id_act)\
