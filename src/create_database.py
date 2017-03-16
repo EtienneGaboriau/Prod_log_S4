64b7912de11675a37748281197d88ef6a7b7b79f
Etienne GABORIAU <E155693G@U-INF-J-C002-09.iut-nantes.univ-nantes.prive>
2017-03-16 10:41:53 +0100

    database finale

--- src/create_database.py
+++ src/create_database.py
@@ -3,33 +3,33 @@ import mysql.connector as mysql
 try:
     conn = mysql.connect(user='E155693G',password='E155693G',host='infoweb', database='E155693G')
     cursor = conn.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS installation (\
-                        id int(10) NOT NULL AUTO_INCREMENT,\
+    cursor.execute("CREATE TABLE installations (\
+                        num_instal int(10) DEFAULT NULL,\
                         nom varchar(50) DEFAULT NULL,\
                         code_postal int(5) DEFAULT NULL,\
                         ville varchar(40) DEFAULT NULL,\
                         latitude decimal(23,20) DEFAULT NULL,\
                         longitude decimal(25,20) DEFAULT NULL,\
-                        PRIMARY KEY(id)\
+                        PRIMARY KEY(num_instal)\
                     )")
-    cursor.execute("CREATE TABLE IF NOT EXISTS equipements (\
-                        id int(10) NOT NULL AUTO_INCREMENT,\
+    cursor.execute("CREATE TABLE equipements (\
+                        id_equip int(10) DEFAULT NULL,\
                         nom varchar(50) DEFAULT NULL,\
                         num_instal int(5) DEFAULT NULL,\
-                        PRIMARY KEY(id),\
-                        FOREIGN KEY(num_instal) REFERENCES installation(id)\
+                        PRIMARY KEY(id_equip),\
+                        FOREIGN KEY(num_instal) REFERENCES installations(num_instal)\
                     )")
-    cursor.execute("CREATE TABLE IF NOT EXISTS activites (\
-                        id int(10) NOT NULL AUTO_INCREMENT,\
+    cursor.execute("CREATE TABLE activites (\
+                        id_act int(10) DEFAULT NULL,\
                         nom varchar(50) DEFAULT NULL,\
-                        PRIMARY KEY(id)\
+                        PRIMARY KEY(id_act)\
                     )")
-    cursor.execute("CREATE TABLE IF NOT EXISTS equip_act (\
-                        id_equip int(10) NOT NULL AUTO_INCREMENT,\
+    cursor.execute("CREATE TABLE equip_act (\
+                        id_equip int(10) DEFAULT NULL,\
                         id_act int(10) DEFAULT NULL,\
                         PRIMARY KEY(id_equip, id_act),\
-                        FOREIGN KEY(id_equip) REFERENCES equipements(id),\
-                        FOREIGN KEY(id_act) REFERENCES activites(id)\
+                        FOREIGN KEY(id_equip) REFERENCES equipements(id_equip),\
+                        FOREIGN KEY(id_act) REFERENCES activites(id_act)\
                     )")
 
 
